0ad670b669efc88735b78777ea45c0c1ef756a14
SWD Hussin <hussin.hidar@iu-study.org>
2025-08-30 02:43:42 +0200

    p1

---
+++ src/script.js
@@ -0,0 +1,14 @@
+
+// Bootstrap Burger-Menü automatisch schließen nach Klick auf Link
+document.addEventListener('DOMContentLoaded', function() {
+  var navbarCollapse = document.getElementById('navbarNav');
+  var navLinks = navbarCollapse.querySelectorAll('.nav-link');
+  var bsCollapse = new bootstrap.Collapse(navbarCollapse, {toggle: false});
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+      if (window.getComputedStyle(document.querySelector('.navbar-toggler')).display !== 'none') {
+        bsCollapse.hide();
+      }
+    });
+  });
+});
